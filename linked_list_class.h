c04b56d64a6adad6a244ed8da1bec8d44adc7981
Matvey1338 <duo163@yandex.ru>
2024-11-28 04:43:09 +0400

    Добавьте файлы проекта.

---
+++ linked_list_class.h
@@ -0,0 +1,36 @@
+﻿#pragma once 
+#include <iostream>
+
+
+using namespace std;
+
+
+template <typename T>
+struct Node {
+    T value;
+    Node* next;
+
+    Node(const T& value, Node<T>* next = nullptr) : value(value), next(nullptr) {}
+};
+
+template <typename T>
+class CircularLinkedList {
+private:
+    Node<T>* _head;
+
+public:
+
+	Node<T>* get_head() const {
+		return _head;
+	}
+
+	LinkedList() : _head(nullptr) {}
+
+	LinkedList(const LinkedList& list) : _head(nullptr) {
+		auto ptr = list._head;
+		while (ptr) {
+			push_tail(ptr->value);
+			ptr = ptr->next;
+		}
+	}
+};
---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿// lab2.cpp: определяет точку входа для приложения.
+//
+
+#include "lab2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
