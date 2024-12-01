a2dd03f039709b800b2efd2f65d89593b6cb93fa
Matvey1338 <duo163@yandex.ru>
2024-12-01 18:23:07 +0400

    done

--- linked_list_class.h
+++ linked_list_class.h
@@ -1,6 +1,6 @@
 ﻿#pragma once 
 #include <iostream>
-
+#include <random>
 
 using namespace std;
 
@@ -10,27 +10,272 @@ struct Node {
     T value;
     Node* next;
 
-    Node(const T& value, Node<T>* next = nullptr) : value(value), next(nullptr) {}
+    Node(const T& value, Node<T>* next = nullptr) : value(value), next(next) {}
 };
+
 
 template <typename T>
 class CircularLinkedList {
 private:
-    Node<T>* _head;
+    Node<T>* _tail;
 
 public:
+    Node<T>* get_head() const {
+        return _tail->next;
+    }
 
-	Node<T>* get_head() const {
-		return _head;
-	}
+    CircularLinkedList() : _tail(nullptr) {}
 
-	LinkedList() : _head(nullptr) {}
+	CircularLinkedList(const CircularLinkedList& list) : _tail(nullptr) {
+		if (list->empty()) {
+			return;
+		}
 
-	LinkedList(const LinkedList& list) : _head(nullptr) {
-		auto ptr = list._head;
-		while (ptr) {
+		Node<T>* ptr = list._tail->next;
+		do {
 			push_tail(ptr->value);
 			ptr = ptr->next;
-		}
+		} while (ptr != _tail->next);
+	} 
+
+	CircularLinkedList(const size_t size) {};
+
+	CircularLinkedList<T>& operator=(const CircularLinkedList<T>& lst) {
+        if (this != &lst) {
+            clear();
+            if (!lst.empty()) {
+                Node* current = lst._tail->next;
+                do {
+                    push_tail(current->data);
+                    current = current->next;
+                } while (current != lst._tail->next);
+            }
+        }
+        return *this;
 	}
+
+	~CircularLinkedList() {
+        clear();
+	}
+
+
+	bool empty() const {
+		return _tail == nullptr;
+	}
+
+    void clear() {
+        if (empty()) {
+            return;
+        }
+
+        Node<T>* current = _tail->next; // Указатель на первый элемент
+        Node<T>* nextNode = nullptr;
+
+        while (current != _tail) { // Пока не дойдём до хвоста
+            nextNode = current->next; // Сохраняем следующий узел
+            std::cout << "Deleting node at address: " << current << std::endl;
+            delete current; // Удаляем текущий узел
+            current = nextNode; // Переходим к следующему узлу
+        }
+
+        // Удаляем последний узел (хвост)
+        std::cout << "Deleting node at address: " << current << std::endl;
+        delete current;
+
+        _tail = nullptr; // Список теперь пуст
+    }
+
+    void push_tail(const T& value) {
+        Node<T>* newNode = new Node<T>(value, nullptr);
+        if (empty()) {
+            _tail = newNode;
+            _tail->next = _tail;
+        }
+        else {
+            newNode->next = _tail->next;
+            _tail->next = newNode;
+            _tail = newNode;
+        }
+    }
+
+    void push_tail(const CircularLinkedList<T>& list) {
+        if (list->empty())
+            return; 
+
+        Node<T>* ptr = list->_tail->next;
+        do {
+            push_tail(ptr->value);
+            ptr = ptr->next;
+        } while (ptr != list->_tail->next);
+    }
+
+    void push_head(const T value) {
+        Node<T>* new_node = new Node<T>(value, nullptr);
+
+        if (empty()) {
+            _tail = new_node;
+            _tail->next = _tail;
+        }
+        else {
+            new_node->next = _tail->next;
+            _tail->next = new_node;
+        }
+    }
+
+    void push_head(const CircularLinkedList<T>& list) {
+        if (list->empty())
+            return;
+
+        Node<T>* ptr = list->_tail->next;
+        do {
+            push_head(ptr->value);
+            ptr = ptr->next;
+        } while (ptr != list->_tail->next);
+    }
+
+    void pop_head() {
+        if (empty()) {
+            throw out_of_range("Linked list is empty");
+        }
+        if (this->_tail == this->_tail->next) {
+            delete _tail;
+            _tail = nullptr;
+            return;
+        }
+        Node<T>* ptr = _tail->next;
+        _tail->next = ptr->next;
+        delete ptr;
+
+    }
+
+    void pop_tail() {
+        if (empty()) {
+            throw out_of_range("Linked list is empty");
+        }
+        if (this->_tail == this->_tail->next) {
+            delete _tail;
+            _tail = nullptr;
+            return;
+        }
+        Node<T>* ptr = _tail->next;
+        while (ptr->next != _tail) {
+            ptr = ptr->next;
+        }
+        ptr->next = _tail->next;
+        delete _tail;
+        _tail = ptr;
+    }
+
+    void delete_node(const T& value) {
+        if (empty()) {
+            return; 
+        }
+
+        Node<T>* current = _tail->next;
+        Node<T>* prev = _tail;
+
+  
+        do {
+            if (current->value == value) {
+                Node<T>* to_delete = current;
+                prev->next = current->next;
+
+
+                if (current == _tail) {
+                    _tail = (current == current->next) ? nullptr : prev; 
+                }
+
+                current = current->next;
+                delete to_delete;
+            }
+            else {
+                prev = current;
+                current = current->next;
+            }
+        } while (current != (_tail ? _tail->next : nullptr));
+
+    }
+
+    T operator[](size_t index) const {
+        if (index < 0 || index >= this->size()) {
+            throw out_of_range("Index is out of range");
+        }
+        Node<T>* ptr = _tail->next;
+        for (int i = 0; i < index; i++) {
+            ptr = ptr->next;
+        }
+        return ptr->value;
+    }
+
+    T& operator[](size_t index) {
+        if (index < 0 || index >= this->size()) {
+            throw out_of_range("Index is out of range");
+        }
+        Node<T>* ptr = _tail->next;
+        for (int i = 0; i < index; i++) {
+            ptr = ptr->next;
+        }
+        return ptr->value;
+    }
+
+    size_t size() const {
+        Node<T>* ptr = _tail->next;
+        size_t size = 0;
+        if (empty()) {
+            size = 0;
+            return size;
+        }
+        do {
+            ptr = ptr->next;
+            size++;
+        } while (ptr != _tail->next);
+        return size;
+    }
+
 };
+
+template <typename T>
+ostream& operator<<(ostream& a, const CircularLinkedList<T>& list) {
+    if (list.empty()) {
+        a << "List is empty";
+        return a;
+    }
+
+    Node<T>* ptr = list.get_head();
+    Node<T>* start = ptr;
+
+    do {
+        a << ptr->value << " ";
+        ptr = ptr->next;
+    } while (ptr != start);
+
+    return a;
+}
+
+template <>
+CircularLinkedList<int>::CircularLinkedList(size_t size) : _tail(nullptr) {
+    for (int i = 0; i < size; i++) {
+        this->push_tail(rand());
+    }
+}
+
+template <>
+CircularLinkedList<bool>::CircularLinkedList(size_t size) : _tail(nullptr) {
+    for (int i = 0; i < size; i++) {
+        this->push_tail(rand() % 2);
+    }
+}
+
+template <>
+CircularLinkedList<float>::CircularLinkedList(size_t size) : _tail(nullptr) {
+    for (int i = 0; i < size; i++) {
+        this->push_tail(rand() + 0.00001 * rand());
+    }
+}
+
+template <>
+CircularLinkedList<double>::CircularLinkedList(size_t size) : _tail(nullptr) {
+    for (int i = 0; i < size; i++) {
+        this->push_tail(rand() + 0.00001 * rand());
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,12 +1,69 @@
 ﻿// lab2.cpp: определяет точку входа для приложения.
 //
 
-#include "lab2.h"
+#include "linked_list_class.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	srand(static_cast<unsigned int>(time(nullptr)));
+	cout << "Lab 2 Tests" << endl << "----------------------" << endl;
+
+	cout << "Circular Linked List Tests" << endl << "----------------------" << endl << endl;
+
+	CircularLinkedList<int> list1(3);
+	CircularLinkedList<float> list2(3);
+	CircularLinkedList<double> list3(3);
+	CircularLinkedList<bool> list4(3);
+	cout << "random int list: " << list1 << endl;
+	cout << "random float list: " << list2 << endl;
+	cout << "random double list: " << list3 << endl;
+	cout << "random bool list: " << list4 << endl << "----------------------" << endl << endl;
+
+	CircularLinkedList<int> list5(list1);
+	cout << "copy constructor: " << list5 << endl;
+
+	CircularLinkedList<int> list6;
+	list6 = list1;
+	cout << "equality operator: " << list5 << endl << "----------------------" << endl << endl;
+
+	list1.push_tail(100000000);
+	cout << "push_tail: " << list1 << endl;
+
+	list1.pop_tail();
+	cout << "pop_tail: " << list1 << endl;
+
+	list1.push_head(100000000);
+	cout << "push_head: " << list1 << endl;
+
+	list1.pop_head();
+	cout << "pop_head: " << list1 << endl;
+
+	list6.push_tail(list1);
+	cout << "push_tail list: " << list6 << endl;
+
+	list6.push_tail(list1);
+	cout << "push_tail list: " << list6 << endl;
+
+	list6.push_head(list1);
+	cout << "push_head list: " << list6 << endl << "----------------------" << endl << endl;
+
+	CircularLinkedList<bool> list7(10);
+	cout << "list7: " << list7 << endl;
+	list7.delete_node(true);
+	cout << "delete_node in list7 for true: " << list7 << endl;
+
+	list6.delete_node(41);
+	cout << "delete_node in list6 for 41: " << list6 << endl;
+
+
+	cout << "operator [] read: " << list1[0] << endl;
+	list1[0] = 100000000;
+	cout << "operator [] write: " << list1[0] << endl;
+
+	cout << "list1.size(): " << list1.size() << endl;
+
+
 	return 0;
 }
